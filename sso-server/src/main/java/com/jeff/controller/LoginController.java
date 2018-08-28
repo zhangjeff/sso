8784b3dada9a6058f4214651d0cfe5200099bdd3
youpeng.zhang <youpeng.zhang@chinaredstar.com>
2018-08-28 16:42:10 +0800

    commit

--- sso-server/src/main/java/com/jeff/controller/LoginController.java
+++ sso-server/src/main/java/com/jeff/controller/LoginController.java
@@ -2,8 +2,7 @@ package com.jeff.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by zhangying on 2018/8/28.
@@ -13,9 +12,23 @@ public class LoginController {
 
 
     @RequestMapping("/login")
-    public String hello(Model model) {
+    public String login(Model model) {
 //        model.addAttribute("name", "world333");
 
         return "template/login";
     }
+
+    @RequestMapping(value = "/submitLogin", method = {RequestMethod.POST})
+    public String submitLogin(Model model,
+                              @RequestParam(value = "account") String account,
+                              @RequestParam(value = "password") String password) {
+//        model.addAttribute("name", "world333");
+        System.out.println("account =" + account);
+        System.out.println("password =" + password);
+
+        String user = account + password;
+
+        return "template/admin";
+    }
+
 }
